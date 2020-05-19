9ff08c76a735e7645e988c1b75b45fb3c195ba7a
gladdos <gladdos97@gmail.com>
2020-05-19 17:55:32 +0200

    Update fizzBuz.py

--- fizzBuz.py
+++ fizzBuz.py
@@ -2,14 +2,15 @@ import sys
 
 player_active = True
 loop_counter = 1
-two_counter = 0
 
 def winner_announcer(loop_counter):
     if loop_counter % 2 == 0:
         print("Player 1 wins!!!") #Cant use return because of sys.exit
+
         sys.exit(0)
     else:
         print("Player 2 wins!!!") #Cant use return because of sys.exit
+
         sys.exit(0)
 
     # if it ends on 1 player 1 loses
@@ -32,9 +33,9 @@ def fizzbuzz_compiler(step):
 
 def input_check(player_input, loop_counter):
     """Takes in player_input and checks it against fizzbuzz compiler"""
-    if player_input == fizzbuzz_compiler(loop_counter): # if 1 = 1(1) ???
+    if player_input == fizzbuzz_compiler(loop_counter): # if 1 = 1(1) ??? ## Issue is that the p1_input is still a STR instead of being an INT.
         loop_counter += 1
-        return loop_counter # had to change it from return to if
+        return loop_counter  # had to change it from return to if
     else:
         return False
 
@@ -46,12 +47,12 @@ while loop_counter < 6:  # Main game loop
             winner_announcer(loop_counter)
             # Run end game function and declare winner
         else:
-            loop_counter+=1
+            loop_counter += 1
         # continue loop and pass to player 2
         if loop_counter >= 6:  # Loop catcher or whatever ## Find a way to improve this
             player_active = False
             break
-        p2_input = int(input("Enter your number player 2: "))
+        p2_input = input("Enter your number player 2: ")
         if input_check(p2_input, loop_counter) == False:
             winner_announcer(loop_counter)
         else:
