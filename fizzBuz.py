071d4e27f58842a6748f7807f0eee33553bbdf32
gladdos <gladdos97@gmail.com>
2020-05-20 16:40:17 +0200

    Update fizzBuz.py

--- fizzBuz.py
+++ fizzBuz.py
@@ -12,17 +12,6 @@ def winner_announcer(loop_counter):
         print("Player 2 wins!!!") #Cant use return because of sys.exit
         sys.exit(0)
 
-    # if it ends on 1 player 1 loses
-    # if it ends on 2 player 2 loses
-
-    #The winner_announcer() is using the % of loop_counter info to decide who's the winner.
-
-    #Yeah we'll use a trick to handle the string or int input. I'm not sure if you have used it yet.
-    # We use try: to convert code to int. If code is not a number that will raise an Exception ValueError.
-    # We use except: to catch this error and have code for handling strings inside of it
-    #[8:43 PM]
-    #Let me know when you're done
-
 def fizzbuzz_compiler(step):
     """compiles latest expected result of fizzbuzz"""
     if step % 3 == 0 and step % 5 == 0:
@@ -34,7 +23,6 @@ def fizzbuzz_compiler(step):
     else:
         return step
 
-
 def input_check(player_input, loop_counter):
     """Takes in player_input and checks it against fizzbuzz compiler"""
     if player_input == fizzbuzz_compiler(loop_counter): # if 1 = 1(1) ??? ## Issue is that the p1_input is still a STR instead of being an INT.
@@ -42,7 +30,6 @@ def input_check(player_input, loop_counter):
         return loop_counter  # had to change it from return to if
     else:
         return False
-
 
 while loop_counter < 6:  # Main game loop
     if player_active == True:
@@ -60,7 +47,7 @@ while loop_counter < 6:  # Main game loop
         else:
             loop_counter += 1
         # continue loop and pass to player 2
-        if loop_counter >= 6:  # Loop catcher or whatever ## Find a way to improve this
+        if loop_counter >= 6:
             player_active = False
             break
         try:
@@ -74,8 +61,3 @@ while loop_counter < 6:  # Main game loop
         else:
             loop_counter += 1
     player_active = True
-
-# FizzBuzz
-# Things to do:
-#   Create game winner function that announces winner and ends game
-#   Finish the game loop for player 2
