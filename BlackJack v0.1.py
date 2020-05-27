70a16c78b99f79c463768a7b70029fea3aa94cf2
gladdos <gladdos97@gmail.com>
2020-05-27 07:36:10 +0200

    update

--- BlackJack v0.1.py
+++ BlackJack v0.1.py
@@ -1,29 +1,21 @@
-#BlackJack v0.1
-#TODO: Make a Welcome class, that Greets the user by their name, while also stating their balance.
-#TODO: Make a "Deck" class, (Will be added later)
-
-
-# suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-# ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-# values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
-#          'Queen':10, 'King':10, 'Ace':11}
-
 import random
+ 
+#TODO: Move Welcome() class to main game loop, only make classes for Deck & Player hands
 
 class Deck():
-    def __init__(self):
-        self.cards =[2,3,4,5,6,7,8,9,10*3,11] * 4 #This is a deck consisted of Numbers only
-                                                  # Improve this waaay later
-    def shuffle(self):
-        random.shuffle(self.cards)
-        return self.cards
-
-class Welcome(Deck): # Class that Greets the player & states their balance. ##TODO: Use JSON for balance (or mySQL if you hate yourself)
+    def __init__(self,cards):
+        self.cards = [2,3,4,5,6,7,8,9,10,10,10,10,11] * 4
+ 
+    def deck_shuffle(self):
+        return random.shuffle(self.cards)
+ 
+ 
+class Welcome(): # Class that Greets the player & states their balance. ##TODO: Use JSON for balance (or mySQL if you hate yourself)
     def __init__(self, player_name, player_balance):
-        super().__init__(cards)
         self.player_name = player_name
         self.player_balance = player_balance
-        self.cards = cards
+        self.player_hand = deck(cards).pop(1)*2
+        self.dealer_hand = deck(cards).pop(1)*2
     def betting(self):
         while True:
             self.bet = int(input("How much do you want to bet: "))
@@ -36,26 +28,53 @@ class Welcome(Deck): # Class that Greets the player & states their balance. ##TO
                     break
                 else:
                     print("You answered incorrectly, exiting...")
-                    return self.player_balance
+                    return f"You sucessfully bet: {self.bet}, your balance is: {self.player_balance}"
             else:
                 self.player_balance -= self.bet
-                return self.player_balance
-        self.player_balance -= self.bet
-    
-    def deal():
-        pass
-
-
+                return f"You sucessfully bet: {self.bet}, your balance is: {self.player_balance}"
+        return f"Your balance is: {self.player_balance}"
+ 
+    def deal_player(self):
+        return self.player_hand
+ 
+    def deal_dealer(self):
+        return f"{self.dealer_hand}"
+ 
     def hit(self):
-        pass
-        
-    
+        self.player_hand += deck(cards).pop(1)
+        return self.player_hand
+ 
+    #Add double in future
+ 
     def __str__(self):
         return(f"Welcome to BlackJack, {self.player_name}, your current balance is {self.player_balance}.")
-
-
-        
-player_balance = 1500
+ 
+ 
+player_balance = 1500 #put in simple func that is access whenever game needs to change player's total balance (after winning or losing)
 wlc = Welcome("Nameless One", player_balance)
+print(wlc.deal_player())
+print(wlc.deal_dealer())
+player_active = True
+if player_active == False:
+    break
 print(wlc.betting())
-next_move = input("Would you like to hit or stand")
+while player_active == True: #Game begins
+    next_move = input("Would you like to hit or stand: ")
+    if next_move[0].lower() == "h":
+        if sum(wlc.deal_player()) != 21 and sum(wlc.deal_player()) > 21:
+            print(wlc.hit())
+            break
+        else:
+            print("You lost") #Improve with function for losing
+            break
+    elif next_move[0].lower() == "s":
+ 
+        '''``` SEMI PSUEDO CODE STARTS HERE ```'''
+        # while player_hand is not 21 and player_hand is not > 21:
+        #     while dealer_hand < 17:
+        #         dealer_hand += deck(cards).pop(1)
+        #         if dealer_hand >= 17 and dealer_hand > player_hand:
+        #             print("Dealer wins!!!")
+        #         elif dealer_hand >= 17 and dealer_hand < player_hand:
+        #             print("Player wins")
+        #             player_balance += bet * 2
