194fa625dfb981bc608fabaa9a2ca0844f78ae8b
gladdos <gladdos97@gmail.com>
2020-06-15 23:43:11 +0200

    Update numExpandedForm2.py
    Working version

--- numExpandedForm2.py
+++ numExpandedForm2.py
@@ -1,35 +1,26 @@
+#Refactor meh <3
 def expanded_form(num):
     i = 0
     j = 1
+    h = 0
     whole_number = int(num) # Whole number
-    text = f"{whole_number}"
+    text = ""
     if whole_number == 0: # If first value of num == 0
         text = text[1:]
     num = str(num)[str(num).find("."):]
     num = str(num).replace(".", "")
     ten_list = [1,10,100,1000,10000,100000,1000000,10000000,100000000,1000000000] # Fix this
     z = (len(str(whole_number)) - 1)
-
-
-
-    # if whole_number > 9:# Left side, before "."
-    #     text = ""
-    #     text = f"{(int(whole_number)) // (ten_list[z]) * (ten_list[z])}"
-    #     while whole_number > 9:
-    #         while whole_number > 9:
-    #             if text != "":
-    #                 text+= " + "
-    #             text += f"{(int(whole_number)) // (ten_list[z]) * (ten_list[z])} "
-    #             whole_number = str(whole_number)[1:]
-    #             whole_number = int(whole_number)
-    #             z += 1
-    #         whole_number = whole_number / ten_list[z]
-
-    ### ^^^THIS WHOLE THING IS GARBAGE, BURN IT ^^^####
-           
-
-    counter = len(num)
-    while i < counter: #Right side, after "."
+    if whole_number > 9:# Left side, before "."
+        whole_number = (str(whole_number))
+        if text != "":
+            text+= " + "
+            text += whole_number[0] + (len(whole_number)-1) * "0"
+            whole_number = (whole_number[1:])
+    elif whole_number != 0:
+        text = f"{whole_number}"
+    counter = len(num) #Right side, after "."
+    while i < counter: 
         if num[i] == "0":
             i+=1
             j+=1
@@ -41,8 +32,6 @@ def expanded_form(num):
         j += 1
     return text
 
-num = 134.34
+num = 100.34
 print(expanded_form(num))
-
-
 #TODO : #left_side, right_side = str(num).split('.') REFACTOR
